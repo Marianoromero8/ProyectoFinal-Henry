ff891ea83c5056ddcc8b5534e1f55815e463dfa0
Marianoromero8 <marianoromero97.mr@gmail.com>
2024-07-23 19:59:46 -0300

    deploying

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,9 @@
-import {createStore, applyMiddleware} from 'redux';
-import reducer from './reducer';
-import {thunk} from 'redux-thunk';
-import {composeWithDevTools} from 'redux-devtools-extension'
+import { createStore, applyMiddleware } from 'redux';
+import reducer from './reducer'; 
+import thunk from 'redux-thunk'; 
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
+const store = createStore(reducer,composeWithDevTools(applyMiddleware(thunk))
+);
 
 export default store;
