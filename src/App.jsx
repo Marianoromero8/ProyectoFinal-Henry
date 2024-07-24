610fa526487f2b6be7dec55a6dfd4aed11ccc2b9
Marianoromero8 <marianoromero97.mr@gmail.com>
2024-07-23 21:04:46 -0300

    Front edit

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Landing from './components/Landing/Landing'
 import Home from './components/Home/Home';
 import Details from './components/Details/Details';
 import AboutUs from './components/AboutUs/AboutUs';
+import Form from './components/Form/Form';
 
 // npm run dev ==> en la terminal dentro de la carpeta 'vite-project' para correr el front
 
