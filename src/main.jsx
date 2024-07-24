a24456d6d528869bf33aff9aca2ba0318689052a
Marianoromero8 <marianoromero97.mr@gmail.com>
2024-07-23 22:28:32 -0300

    HashRouter change

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,15 @@ import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
   </Provider>
 )
 
