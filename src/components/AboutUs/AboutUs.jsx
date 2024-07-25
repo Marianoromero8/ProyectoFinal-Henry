78fa14db79ad09e6aceb43467523c46bb1b0a077
Gonzalo.A.Mart <cuentasdechalo@gmail.com>
2024-07-25 15:42:54 -0300

    aditamentos

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Link } from 'react-router-dom';
+import Loader from '../Loader/Loader';
 
 const AboutUs = () => {
   return (
     <div>
-      <h1>Developing...</h1>
+      <h1>Developing...ㅤㅤㅤ<Loader />
+      </h1> 
       <Link to="/Home">
         <button>Go to Home</button>
       </Link>
