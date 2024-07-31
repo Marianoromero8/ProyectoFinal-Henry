86ed0615ccf898e3ad88300f8e0a49c633168d2b
Marianoromero8 <marianoromero97.mr@gmail.com>
2024-07-31 16:52:07 -0300

    Auth developing

--- src/components/Admin/Dashboard/Dashboard.jsx
+++ src/components/Admin/Dashboard/Dashboard.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { loginUser } from "../../../store/slice/authThunks";
+import { loginUser, logoutUser } from "../../../store/slice/authThunks";
+import { useNavigate } from "react-router-dom";
 
 
 const Dashboard = () => {
-
+  const navigate = useNavigate();
   const dispatch = useDispatch();
   const { user, loading, error } = useSelector((state) => state.auth);
 
@@ -13,9 +14,20 @@ const Dashboard = () => {
     dispatch(loginUser());
   }, [dispatch]);
 
+  const handleLogout = async () => {
+    try {
+      logoutUser();
+      dispatch(logoutUser());
+      navigate('/login');
+    } catch (error) {
+      console.error("Error al cerrar sesión:", error);
+    }
+  };
+
   return (
     <div>
       <h2>Welcome to the Dashboard</h2>
+      <button onClick={handleLogout}>Log Out</button>
       {loading && <p>Loading...</p>}
       {user && (
         <div>
